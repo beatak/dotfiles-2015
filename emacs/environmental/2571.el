e0c001fc803e0aa6415a2f680a85219f95db24c5
Takashi Mizohata <beatak@gmail.com>
2014-01-27 16:18:39 -0800

    adding 2571

---
+++ emacs/environmental/2571.el
@@ -0,0 +1,27 @@
+;;; for 2571 (etsy's computer)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(css-indent-offset 4)
+ '(custom-enabled-themes (quote (twilight-anti-bright)))
+ '(custom-safe-themes (quote ("9ea054db5cdbd5baa4cda9d373a547435ba88d4e345f4b06f732edbc4f017dc3" "57072d797dc09fcf563051a85a29d6a51d6f2b1a602e029c35b05c30df319b2a" default)))
+ '(indent-tabs-mode nil)
+ '(js2-basic-offset 4)
+ '(js2-bounce-indent-p t)
+ '(tab-width 4)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(maximize-frame)
+(require 'e2wm)
+
+
+
